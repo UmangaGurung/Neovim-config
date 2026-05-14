62517dbf34ebda89e350b08efd8e8e31f27a95a4
Umanga <umanga.gurung63@gmail.com>
2026-05-14 15:06:58 +0545

    blink and snip config

--- lua/plugins/go-lsp.lua
+++ lua/plugins/go-lsp.lua
@@ -70,12 +70,19 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        go = { "goimports", "gofumpt" },
+        go = { "goimports" },
       },
     },
   },
   {
     "mfussenegger/nvim-lint",
+    optional = true,
+    dependencies = {
+      {
+        "mason-org/mason.nvim",
+        opts = { ensure_installed = { "golangci-lint" } },
+      },
+    },
     opts = {
       linters_by_ft = {
         go = { "golangcilint" },
